41123ceb3929cb788fa5b38114f233adcc860f71
Mrteathyme <Mrteathyme@gmail.com>
2023-10-17 07:59:31 +1100

    yeet skeet weightings are neat

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,41 @@
+use std::ops::Add;
+
 use std::collections::HashMap;
 use rand::{thread_rng, Rng};
 use rand::seq::SliceRandom;
+
+use rand::prelude::*;
+use rand::distributions::WeightedIndex;
+
 
 use std::fs::File;
 use std::io::prelude::*;
 
 use bmp_rust::bmp::BMP;
 
+static DIRECTIONS: [Position; 8] = [
+                                    Position(-1,1,0), Position(0,1,0), Position(1,1,0),
+                                    Position(-1,0,0)                 , Position(1,0,0),
+                                    Position(-1,-1,0),Position(0,-1,0),Position(1,-1,0)
+                                   ];
+
 fn main() {
-    let mut tile_atlas = HashMap::new();
-    //tile_atlas.insert((0,0), TileType::Water);
-    for x in 0..128 {
+    let stack = vec![];
+    let mut tile_atlas: HashMap<Position, Tile> = HashMap::new();
+    for x in 0..128 { 
         for y in 0..72 {
-            //if (x,y) == (0,0) { continue }
-            tile_atlas.insert((x,y), collapse_tile(&tile_atlas, x, y));
+            let mut tile = Tile::new(Position(x,y,0));
+            tile.collapse(&tile_atlas, &stack);
+            tile_atlas.insert(Position(x,y,0), tile);
         }
     }
-    //for tile in tile_atlas.clone() {
-    //    tile_atlas.insert(tile.0, collapse_tile(&tile_atlas, tile.0.0, tile.0.1));
-    //}
-    //tile_atlas.insert((0,0), collapse_tile(&tile_atlas, 0, 0));
+
     let mut file = File::create("map.txt").unwrap();
     let mut string = String::new();
     let mut img = BMP::new(720, 1280, None);
     for x in 0..128 {
         for y in 0..72 {
-            string.push(match tile_atlas.get(&(x as i32,y as i32)).unwrap() {
+            string.push(match tile_atlas.get(&Position(x, y, 0)).unwrap().tile_type {
                 TileType::Grass => 'g',
                 TileType::Sand => 's',
                 TileType::Tree => 't',
@@ -34,7 +44,7 @@ fn main() {
                 TileType::None => ' ',
                 TileType::Invalid => '?',
            });
-            let color: [u8; 4] = match tile_atlas.get(&(x as i32, y as i32)).unwrap() {
+            let color: [u8; 4] = match tile_atlas.get(&Position(x,y,0)).unwrap().tile_type {
                 TileType::Grass => [0,255,0,255],
                 TileType::Water => [0,0,255,255],
                 TileType::Sand => [255,255,0,255],
@@ -42,7 +52,7 @@ fn main() {
                 TileType::House => [245,40,145,255],
                 _ => [0,0,0,255]
             };
-            img.draw_rectangle(Some(color), Some(color), [(x*10) as u16, (y*10 as u16)], [((x+1)*10-1) as u16, ((y+1)*10-1) as u16]);
+            img.draw_rectangle(Some(color), Some(color), [(x*10) as u16, (y*10) as u16], [((x+1)*10-1) as u16, ((y+1)*10-1) as u16]);
         }
         string.push('\n');
     }
@@ -50,58 +60,89 @@ fn main() {
     let _ = img.save_to_new("img.bmp");
 }
 
+#[derive(Clone, Copy, Hash, Eq, PartialEq, Debug)]
+struct Position(i32,i32,i32); 
 
-struct Tile<const DOMAIN_SIZE: usize> {
-    tile_type: TileType,
-    x_pos: i32,
-    y_pos: i32,
-    connecting_tiles: [Option<TileType>; DOMAIN_SIZE]
+impl Add for Position {
+    type Output = Self;
+    fn add(self, other:Self) -> Self {
+        Self {
+            0: self.0 + other.0,
+            1: self.1 + other.1,
+            2: self.2 + other.2
+        }
+    }
 }
 
-impl<const DOMAIN_SIZE: usize> Tile<DOMAIN_SIZE> {
-    fn new() -> Tile<DOMAIN_SIZE> {
-       todo!(); 
-    }
-    /*
-    fn collapse(&self, tile_atlas: &HashMap<(i32,i32), TileType>) -> TileType {
-        let domain = self.get_domain(tile_atlas); 
-    }
-    */
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    tile_type: TileType,
+    position: Position,
+    connecting_tiles: [Option<TileType>; 8]
+}
 
-    /*
-    fn update_connecting_tiles(&self, tile_atlas: &HashMap<(i32,i32), TileType>) {
-        let connecting_tile_positions = [(self.x_pos-1,self.y_pos),(self.x_pos,self.y_pos-1),(self.x_pos,self.y_pos+1),(self.x_pos+1,self.y_pos)];
-        for tile in connecting_tile_positions {
-            match tile_atlas.get(&tile) {
-                Some(tile_type)
-                None()
+impl Tile {
+    fn new(position: Position) -> Tile {
+        Tile {
+            tile_type: TileType::None,
+            position,
+            connecting_tiles: [None;8]
+        }
+    }
+    
+    fn collapse(&mut self, tile_atlas: &HashMap<Position, Tile>, stack: &[Tile]) {
+        self.update_connecting_tiles(tile_atlas);
+        let mut choices = vec![];
+        let mut weights = vec![];
+        for (key,value) in self.get_domain_weights() {
+           if value > 0.0 {
+            choices.push(key);
+            weights.push(value);
+           }
+        }
+        if weights.len() == 0 {
+            self.tile_type = TileType::Invalid;
+            return
+        }
+        let dist = WeightedIndex::new(&weights).unwrap();
+        let mut rng = thread_rng();
+        self.tile_type = choices[dist.sample(&mut rng)];
+    }
+
+
+    
+    fn update_connecting_tiles(&mut self, tile_atlas: &HashMap<Position, Tile>) {
+        for (i, direction) in DIRECTIONS.into_iter().enumerate() {
+            self.connecting_tiles[i] = match tile_atlas.get(&(direction+self.position)) {
+                Some(tile) => Some(tile.tile_type),
+                None => None
+            };
+        }
+    }
+
+    fn get_domain_weights(&self) -> HashMap<TileType, f32> {
+        let mut weights = TileType::init_domain_weights();
+        let real_tiles: Vec<TileType> = self.connecting_tiles.into_iter().filter(|&value| value != None).map(|value| value.unwrap()).collect();
+        for (tile, _) in weights.clone() {
+            if tile.can_spawn(&real_tiles) == false {
+                weights.insert(tile, 0.0);
             }
         }
+        weights
     }
-    */
 
-    fn get_domain(&self, tile_atlas: &HashMap<(i32,i32),TileType>) -> Vec<TileType> {
-        let mut domain = TileType::init_domain();
-        let mut domain_keys = vec![];
-        for (key,_) in domain.clone() {
-            domain_keys.push(key);
-        }
-        let connecting_tiles = [(self.x_pos-1,self.y_pos),(self.x_pos,self.y_pos-1),(self.x_pos,self.y_pos+1),(self.x_pos+1,self.y_pos)];
-        for tile in connecting_tiles {
-            if let Some(tile_type) = tile_atlas.get(&tile) {
-                for domain_key in domain_keys.clone() {
-                    if domain.get(&domain_key) == Some(&true) && tile_type.get_domain().contains(&domain_key) == false {
-                        domain.insert(domain_key, false);
-                    }
-                }
+    fn get_entropy(&self) -> f32 {
+        let mut weighting_sum: f32 = 0.0;
+        let mut log_weights_sum: f32 = 0.0;
+        for (tile, weight) in self.get_domain_weights() {
+            if weight > 0.0 {
+                println!("{2} added: {0} to weights sum, {2} added {1} to log_weights", weight, weight*weight.log2(),format!("{:#?}", tile));
+                weighting_sum += weight;
+                log_weights_sum += weight * weight.log2(); 
             }
         }
-        let mut filtered_domain = domain.into_iter().filter(|(key,value)| (value == &true)).map(|(tile_type, _)| tile_type).collect::<Vec<TileType>>();
-        filtered_domain
-    }
-
-    fn get_entropy(&self, tile_atlas: &HashMap<(i32,i32),TileType>) -> i32 {
-        self.get_domain(tile_atlas).len() as i32
+        println!("Weighting sum is: {weighting_sum}  log_weights_sum is: {log_weights_sum}, log of weight sum is: {}", weighting_sum.log2());
+        weighting_sum.log2() - (log_weights_sum / weighting_sum)
     }
 }
 
@@ -161,7 +202,7 @@ fn collapse_tile(tile_atlas: &HashMap<(i32,i32), TileType>, x_pos: i32, y_pos: i
         None => {println!("{:#?}", printable_domain); println!("{:#?}", domain_keys); TileType::Invalid }
     }
 }
-#[derive(Clone, Debug, Hash, Eq, PartialEq)]
+#[derive(Clone, Debug, Hash, Eq, PartialEq, Copy)]
 enum TileType {
     Grass,
     Water,
@@ -278,6 +319,24 @@ impl TileType {
             }
             _ => false
         }
+    }
+
+    fn get_base_weight(&self) -> f32 {
+        match self {
+            Self::House => 0.1,
+            Self::Water => 0.5,
+            Self::Tree => 0.5,
+            Self::Sand => 0.2,
+            _ => 1.0
+        }
+    }
+
+    fn init_domain_weights() -> HashMap<Self,f32> {
+        let mut domain = HashMap::new();
+        for tile in Self::DEFAULT_DOMAINS {
+            domain.insert(tile,tile.get_base_weight()); //ToDo: replace magic number with get_weight function
+        }
+        domain
     }
 
     fn init_domain() -> HashMap<Self, bool> {
